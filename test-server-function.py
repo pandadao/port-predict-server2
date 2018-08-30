25ee8bec7288c731f6ceade60cd4fbadca9d3adc
pandadao <cgsh112@gmail.com>
2018-08-30 20:35:16 +0800

    test function record

---
+++ test-server-function.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import socket, sys
+from thread import *
+
+def threadWork(client):
+    while True:
+            msg = client.recv(1024)
+            if not msg:
+                pass
+
+            else:
+                print "Client send: "+ msg
+                client.send("You say: " + msg + "\r\n")
+
+            client.close()
+
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+except socket.error, msg:
+    sys.stderr.write("[ERROR] %s\n" % msg[1])
+    sys.exit(1)
+
+sock.setsocketopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind((''), 54321)
+sock.listen(5)
+
+while True:
+    (csock, adr) = sock.accept()
+    print "Client Info: " , csock, adr
+    start_new_thread(threadWork, (csock,))
+
+sock.close()
