3952d6ee0a623fdc9c887229724fe00bb9bd06d2
pandadao <cgsh112@gmail.com>
2018-09-01 14:14:49 +0800

    test

--- device-register.py
+++ device-register.py
@@ -8,28 +8,27 @@ And return the device ID and password to the device.
 
 import socket
 import random
-import thread 
+import thread
 
 
-'''
-generate a random password for device, the format is english and number mixed.
-
-def Random_value(string_length = 10):
+# Generate the password for device, it will be english and number mixed.
+def Random_Password(string_length = 10):    #The string_length can change like print(Random_Password(6)), and the length will be 6
     random = str(uuid.uuid4())
     random = random.replace("-", "")
-    return random[0:string_length]
+    Device_password = random[0:string_length]
+    return  Device_password
 
-print(Random_value(6))
-'''
-
-def Random_value(string_length = 10):
-    random = str(uuid.uuid4())
-    random = random.replace("-", "")
-    return random[0:string_length]
+# Generate the device ID, it depends the connection client mac address value.
+def Random_ID(string_length = 10):
+    random = str(Device_MAC)
+    Device_ID = random[0:6]  #datatype is str
+    return Device_ID
 
 
 
 def main():
+    HOST = "0.0.0.0"
+    PORT = 30345
 
 
 if __name__ == "__main__":
